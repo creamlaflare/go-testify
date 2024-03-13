946971b3d08c1d187be6a414227f7d608f007776
creamlaflare <antonkazachenko2002@gmail.com>
2024-03-13 15:19:56 -0700

    Implement testing functions using testify lib

---
+++ main_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestMainHandlerWhenOk(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	assert.Equal(t, http.StatusOK, responseRecorder.Code, "Wrong status code")
+}
+
+func TestMainHandlerWhenUnsupportedCity(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=tula", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code, "Wrong status code")
+
+	assert.Equal(t, "wrong city value", responseRecorder.Body.String(), "wrong response")
+}
+
+func TestMainHandlerWhenLargeCount(t *testing.T) {
+	totalCount := 4
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	assert.Equal(t, http.StatusOK, responseRecorder.Code, "wrong status code")
+
+	body := responseRecorder.Body.String()
+	list := strings.Split(body, ",")
+
+	assert.Equal(t, totalCount, len(list), "wrong expected cafe count")
+}
